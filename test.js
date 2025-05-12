26f64d825d3e15e0d50bf871cd6f630a6460e7d4
atin0291 <sharma.atin57@gmail.com>
2025-05-12 10:35:13 +0530

    Update test.js

--- test.js
+++ test.js
@@ -2,3 +2,13 @@ console.log("this is test file");
 let a = 10;
 a = a+ 12;
 console.log("this is test file12",a);
+console.log("this is test file");
+let a = 10;
+a = a+ 12;
+console.log("this is test file12",a);
+
+console.log("this is test file");
+let a = 10;
+a = a+ 12;
+console.log("this is test file12",a);
+
